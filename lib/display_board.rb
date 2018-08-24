1f7c8b75f7cf77d9662f0eb48ad5474f1ab948fb
Giacomo Sorbi <g.sorbi@gmail.com>
2018-08-24 14:57:27 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,3 @@
 def display_board(board)
   puts board.each_with_index.map{|e,i| (i>0 && i%3==0 ? "\n-----------\n" : "") + " #{e} " + (i%3==2 ? "" : "|")}.join
 end
-puts display_board(["O", "X", " ", " ", "X", " ", "X", "O", " "])
