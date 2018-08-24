24ce83d45157eabb0a384810438d7ef8b713e211
Giacomo Sorbi <g.sorbi@gmail.com>
2018-08-24 14:57:13 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,4 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board(board)
+  puts board.each_with_index.map{|e,i| (i>0 && i%3==0 ? "\n-----------\n" : "") + " #{e} " + (i%3==2 ? "" : "|")}.join
+end
+puts display_board(["O", "X", " ", " ", "X", " ", "X", "O", " "])
